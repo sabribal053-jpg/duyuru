0ecc4cecc5a35584cce8af2f5886b96f1b467134
sabribal053-jpg <sabribal053@gmail.com>
2026-09-14 01:15:40 +0300

    fix: enable Discord voice state intent

--- bot.js
+++ bot.js
@@ -19,7 +19,11 @@ if (missingEnvironment.length > 0) {
 }
 
 const client = new Client({
-  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages],
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.GuildVoiceStates,
+  ],
 });
 
 function reportBackgroundError(source, error) {
